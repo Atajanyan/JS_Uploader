4bd215ae7928e3700d8305a045f484d75812e9bd
Atajanyan <atajanyanh@gmail.com>
2023-08-04 14:02:11 +0400

    change logic and  create parallel uploder

--- script.js
+++ script.js
@@ -1,62 +1,21 @@
 const endpoint = "http://localhost:8080";
 let uploadQueue = [];
+const progressBarFillArr = [];
 let activeUploads = 0;
-let uploads = 0;
 let files = [];
-let nextFilesChange = [];
 let uploadCount = 0;
-let inputChangeCount = 0;
-let handleNextFilesChange = null;
-let finalFiles = [];
+let inputChangeCount = false;
+let sliceCount = 0;
 
 const dropArea = document.querySelector(".dropArea");
 const fileInput = document.querySelector(".fileInput");
 const fileList = document.querySelector(".fileList");
 
-function handleDragOver(event) {
+function handleDragEnter(event) {
   event.preventDefault();
   event.stopPropagation();
   event.dataTransfer.dropEffect = "copy";
   event.target.classList.add("dropArea--border-Black");
-}
-
-function handleFileSelect(event) {
-  event.preventDefault();
-  event.stopPropagation();
-
-  if (files.length) {
-    nextFilesChange = event.target.files || event.dataTransfer.files;
-  } else {
-    files = event.target.files || event.dataTransfer.files;
-  }
-
-  finalFiles = nextFilesChange.length ? nextFilesChange : files;
-  for (let i = 0; i < finalFiles.length; i++) {
-    const file = finalFiles[i];
-    file.id = Math.random();
-    uploadQueue.push(file);
-    createFileItem(file);
-  }
-
-  let progressUploadedFiles = uploadQueue.slice(uploads);
-
-  if (inputChangeCount) {
-    handleNextFilesChange = function () {
-      let progressUploadedFiles1 = uploadQueue.slice(uploads);
-      processUpload(progressUploadedFiles1);
-      uploads += finalFiles.length;
-      uploadCount = 0;
-      inputChangeCount = 0;
-    };
-    return;
-  }
-
-  inputChangeCount = 1;
-
-  if (!uploadCount) {
-    processUpload(progressUploadedFiles);
-    uploads += finalFiles.length;
-  }
 }
 
 function createFileItem(file) {
@@ -78,6 +37,8 @@ function createFileItem(file) {
 
   const progressBarFill = document.createElement("div");
   progressBarFill.className = "progressBarFill";
+  progressBarFill.id = file.id;
+  progressBarFillArr.push(progressBarFill);
 
   description.append(name, percent);
   progressBar.appendChild(progressBarFill);
@@ -85,93 +46,90 @@ function createFileItem(file) {
   fileList.appendChild(fileItem);
 }
 
-async function handleActiveUploads(progressArr, progressUploadedFiles) {
-  for (let j = 0; j < progressArr.length; j++) {
-    const file = progressArr[j];
-    uploadFile(file, progressUploadedFiles);
+function handleFileSelect(event) {
+  event.preventDefault();
+  event.stopPropagation();
+
+  files = event.target.files || event.dataTransfer.files;
+
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+    file.id = Math.random() + Date.now();
+    uploadQueue.push(file);
+    createFileItem(file);
+  }
+
+  event.target.value = ''
+
+  if (!inputChangeCount) {
+    inputChangeCount = true;
+    parallelUpload(uploadQueue);
   }
 }
 
-function processUpload(progressUploadedFiles) {
-  let progressArr = progressUploadedFiles.splice(0, 3);
-  handleActiveUploads(progressArr, progressUploadedFiles);
+function parallelUpload(arr) {
+  let parallelArrSlice = arr.slice(sliceCount, sliceCount + 3);
+
+  uploadFile(parallelArrSlice, uploadQueue.slice(parallelArrSlice.length));
 }
 
-function uploadFile(file, progressUploadedFiles) {
-  const formData = new FormData();
-  formData.append("file", file);
+function uploadFile(arr, parallelArrSlice) {
+  for (let i = 0; i < arr.length; i++) {
+    let file = arr[i];
+    const formData = new FormData();
+    formData.append("file", file);
 
-  const xhr = new XMLHttpRequest();
+    const xhr = new XMLHttpRequest();
 
-  xhr.open("POST", endpoint, true);
+    xhr.open("POST", endpoint, true);
 
-  xhr.upload.onprogress = (event) => {
-    if (event.lengthComputable) {
-      const progress = ((event.loaded / event.total) * 100).toFixed(2);
-      updateProgressBar(file, progress);
-      if (+progress === 100) {
+    const progressUpload = progressBarFillArr.find((el) => el.id == file.id);
+
+    xhr.upload.onprogress = (event) => {
+      if (event.lengthComputable) {
+        const progress = ((event.loaded / event.total) * 100).toFixed(2);
+        updateProgressBar(progress, progressUpload);
+      }
+    };
+
+    xhr.onload = () => {
+      if (xhr.status === 200) {
         activeUploads++;
-        ++uploadCount;
+        uploadCount++;
+        sliceCount++;
       }
-    }
-  };
 
-  xhr.onload = () => {
-    if (activeUploads === 3) {
-      activeUploads = 0;
-      processUpload(progressUploadedFiles);
-    }
-
-    if (uploadCount === (files.length || nextFilesChange.length)) {
-      if (nextFilesChange.length) {
-        if (handleNextFilesChange) {
-          activeUploads = 0;
-          uploadCount = 0;
-          inputChangeCount = 0;
-          nextFilesChange = [];
-          files = [];
-          handleNextFilesChange();
-          handleNextFilesChange = null;
-        } else {
-          inputChangeCount = 0;
-        }
-      } else {
+      if (activeUploads >= 3 || activeUploads === arr.length) {
         activeUploads = 0;
-        uploadCount = 0;
-        inputChangeCount = 0;
-        files = [];
+        parallelUpload(uploadQueue);
       }
-    } else if (uploadCount && !files.length && !nextFilesChange.length) {
-      uploadCount = 0;
-    }
-  };
 
-  xhr.onerror = () => {
-    new Error("Error");
-  };
-
-  xhr.send(formData);
-}
-
-function updateProgressBar(file, progress) {
-  for (let i = 0; i < [...fileList.children].length; i++) {
-    if (+[...fileList.children][i].id === file.id) {
-      const progressBarFill = [...fileList.children][i].querySelector(".progressBarFill");
-      let percent = [...fileList.children][i].querySelector(".fileItem__percent");
-      percent.textContent = `${progress}%`;
-
-      progressBarFill.style.width = `${progress}%`;
-
-      if (progress < 100) {
-        progressBarFill.style.backgroundColor = "#2196f3";
-      } else {
-        progressBarFill.style.backgroundColor = "green";
+      if (uploadCount === uploadQueue.length) {
+        activeUploads = 0;
+        inputChangeCount = false;
       }
-    }
+    };
+
+    xhr.onerror = () => {
+      new Error("Error");
+    };
+
+    xhr.send(formData);
   }
 }
 
-dropArea.addEventListener("dragenter", handleDragOver);
+function updateProgressBar(progress, progressUpload) {
+  progressUpload.style.width = `${progress}%`;
+  
+  
+  if (progress < 100) {
+    progressUpload.style.backgroundColor = "#2196f3";
+  }else{
+    progressUpload.style.backgroundColor = "green";
+  }
+}
+
+dropArea.addEventListener("dragenter", handleDragEnter);
 dropArea.addEventListener("dragover", (e) => e.preventDefault());
 dropArea.addEventListener("drop", handleFileSelect);
 fileInput.addEventListener("change", handleFileSelect);
